c5a6b8651e07048bb5c7cd95d0e3b4642ff5eefc
Patrik Smeds <patrik.smeds@gmail.com>
2022-06-10 09:34:25 +0200

    fix: add missing env file

--- .tests/units/test_cnv_report.py
+++ .tests/units/test_cnv_report.py
@@ -8,12 +8,13 @@ TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 SCRIPT_DIR = os.path.abspath(os.path.join(TEST_DIR, "../../workflow/scripts"))
 sys.path.insert(0, SCRIPT_DIR)
 
-from cnv_report import create_tsv_report
+from cnv_report import create_tsv_report  # noqa
+
 
 class TestGetCaller(unittest.TestCase):
     def test_create_tsv_report(self):
-        cnv = tempfile.mkdtemp() +"/tcvn.txt"
-        create_tsv_report("vcf/test.cnv.vcf", cnv)
+        cnv = tempfile.mkdtemp() + "/tcvn.txt"
+        create_tsv_report(".tests/units/vcf/test.cnv.vcf", cnv)
 
         @dataclass
         class TestCase:
@@ -30,7 +31,7 @@ class TestGetCaller(unittest.TestCase):
                     expected=("testSample_T", "FGFR1", "chr8", "34370199-43930232", "cnvkit", "8.59")
                 ),
                 TestCase(
-                    name = "variant 2",
+                    name="variant 2",
                     expected=("testSample_T", "FGFR1,MYC", "chr8", "35008818-146144253", "gatk_cnv", "7.01")
                 ),
                 TestCase(
