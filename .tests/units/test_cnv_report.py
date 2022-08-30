5dfdc150da0d97e244cdebc5c816729c0251184b
jonca79 <jonas.almlof@igp.uu.se>
2022-08-30 09:37:47 +0200

    test(pytest): missing input params

--- .tests/units/test_cnv_report.py
+++ .tests/units/test_cnv_report.py
@@ -14,7 +14,7 @@ from cnv_report import create_tsv_report  # noqa
 class TestGetCaller(unittest.TestCase):
     def test_create_tsv_report(self):
         cnv = tempfile.mkdtemp() + "/tcvn.txt"
-        create_tsv_report([".tests/units/vcf/test.cnv1.vcf"], [".tests/units/vcf/test.cnv2.vcf"], cnv)
+        create_tsv_report([".tests/units/vcf/test.cnv1.vcf"], [".tests/units/vcf/test.cnv2.vcf"], cnv, 1.5, 0.5)
 
         @dataclass
         class TestCase:
