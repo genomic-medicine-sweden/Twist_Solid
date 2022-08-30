004db2dd7dbd871148da8a9d4fb6ab9a98dc1b00
jonca79 <jonas.almlof@igp.uu.se>
2022-08-30 08:36:02 +0200

    fix(script): output file accessed before opening

--- workflow/scripts/cnv_report.py
+++ workflow/scripts/cnv_report.py
@@ -8,80 +8,77 @@ log = logging.getLogger()
 def create_tsv_report(input_vcfs, input_org_vcfs, output_txt):
     gene_all_dict = {}
     first_vcf = True
-    for input_org_vcf in input_org_vcfs:
-        del_1p19q = {
-            "1p_cnvkit": 0, "19q_cnvkit": 0, "1p_gatkcnv": 0, "19q_gatkcnv": 0,
-            "1p": [0, 125000000, 125000000], "19q": [26500000, 59128983, 32628983],
-        }
-        log.info(f"Opening vcf file: {input_org_vcf}")
-        variants = VariantFile(input_org_vcf)
-        samples = list(variants.header.samples)
-        if len(samples) > 1:
-            raise Exeception(f"Unable to process vcf with more then one sample: {samples}")
-        else:
-            samples = samples[0]
-        for variant in variants:
-            genes = utils.get_annotation_data_info(variant, "Genes")
-            log.debug(f"Processing variant: {variant}")
-            if isinstance(genes, tuple):
-                genes = ",".join(genes)
-            chr = variant.chrom
-            start = variant.pos
-            end = variant.pos + int(utils.get_annotation_data_info(variant, "SVLEN")) - 1
-            callers = utils.get_annotation_data_info(variant, "CALLER")
-            cn = utils.get_annotation_data_info(variant, "CORR_CN")
-            AF = utils.get_annotation_data_info(variant, "Twist_AF")
-            if AF is None:
-                AF = 0.0
-            if cn < 1.2 and chr == "chr1" and start >= del_1p19q["1p"][0] and start <= del_1p19q["1p"][1]:
-                if callers == "cnvkit":
-                    del_1p19q["1p_cnvkit"] += end - start + 1
-                elif callers == "gatk_cnv":
-                    del_1p19q["1p_gatkcnv"] += end - start + 1
-            if cn < 1.2 and chr == "chr19" and start >= del_1p19q["19q"][0] and start <= del_1p19q["19q"][1]:
-                if callers == "cnvkit":
-                    del_1p19q["19q_cnvkit"] += end - start + 1
-                elif callers == "gatk_cnv":
-                    del_1p19q["19q_gatkcnv"] += end - start + 1
-            if genes is not None:
-                for gene in genes.split(","):
-                    if gene not in gene_all_dict:
-                        gene_all_dict[gene] = []
-                        gene_all_dict[gene].append([chr, start, end, callers, cn, AF])
-                    else:
-                        duplicate = False
-                        for variant in gene_all_dict[gene]:
-                            if chr == variant[0] and start == variant[1] and end == variant[2] and callers == variant[3]:
-                                duplicate = True
-                                break
-                        if not duplicate:
+    log.info(f"Opening output tsv file: {output_txt}")
+    with open(output_txt, "w") as writer:
+        for input_org_vcf in input_org_vcfs:
+            del_1p19q = {
+                "1p_cnvkit": 0, "19q_cnvkit": 0, "1p_gatkcnv": 0, "19q_gatkcnv": 0,
+                "1p": [0, 125000000, 125000000], "19q": [26500000, 59128983, 32628983],
+            }
+            log.info(f"Opening vcf file: {input_org_vcf}")
+            variants = VariantFile(input_org_vcf)
+            samples = list(variants.header.samples)
+            if len(samples) > 1:
+                raise Exeception(f"Unable to process vcf with more then one sample: {samples}")
+            else:
+                samples = samples[0]
+            for variant in variants:
+                genes = utils.get_annotation_data_info(variant, "Genes")
+                log.debug(f"Processing variant: {variant}")
+                if isinstance(genes, tuple):
+                    genes = ",".join(genes)
+                chr = variant.chrom
+                start = variant.pos
+                end = variant.pos + int(utils.get_annotation_data_info(variant, "SVLEN")) - 1
+                callers = utils.get_annotation_data_info(variant, "CALLER")
+                cn = utils.get_annotation_data_info(variant, "CORR_CN")
+                AF = utils.get_annotation_data_info(variant, "Twist_AF")
+                if AF is None:
+                    AF = 0.0
+                if cn < 1.2 and chr == "chr1" and start >= del_1p19q["1p"][0] and start <= del_1p19q["1p"][1]:
+                    if callers == "cnvkit":
+                        del_1p19q["1p_cnvkit"] += end - start + 1
+                    elif callers == "gatk_cnv":
+                        del_1p19q["1p_gatkcnv"] += end - start + 1
+                if cn < 1.2 and chr == "chr19" and start >= del_1p19q["19q"][0] and start <= del_1p19q["19q"][1]:
+                    if callers == "cnvkit":
+                        del_1p19q["19q_cnvkit"] += end - start + 1
+                    elif callers == "gatk_cnv":
+                        del_1p19q["19q_gatkcnv"] += end - start + 1
+                if genes is not None:
+                    for gene in genes.split(","):
+                        if gene not in gene_all_dict:
+                            gene_all_dict[gene] = []
                             gene_all_dict[gene].append([chr, start, end, callers, cn, AF])
-        if del_1p19q["1p_cnvkit"] / del_1p19q["1p"][2] > 0.5 and del_1p19q["19q_cnvkit"] / del_1p19q["19q"][2] > 0.5:
-            if first_vcf:
-                writer.write("sample\tgene(s)\tchrom\tregion\tcallers\tnormal_freq\tcopy_number")
-                first_vcf = False
-            writer.write(f"\n{samples}\t1p19q\tNA\tNA\tcnvkit\tNA\tNA")
-        if del_1p19q["1p_gatkcnv"] / del_1p19q["1p"][2] > 0.5 and del_1p19q["19q_gatkcnv"] / del_1p19q["19q"][2] > 0.5:
-            if first_vcf:
-                writer.write("sample\tgene(s)\tchrom\tregion\tcallers\tnormal_freq\tcopy_number")
-                first_vcf = False
-            writer.write(f"\n{samples}\t1p19q\tNA\tNA\tgatk_cnv\tNA\tNA")
+                        else:
+                            duplicate = False
+                            for variant in gene_all_dict[gene]:
+                                if chr == variant[0] and start == variant[1] and end == variant[2] and callers == variant[3]:
+                                    duplicate = True
+                                    break
+                            if not duplicate:
+                                gene_all_dict[gene].append([chr, start, end, callers, cn, AF])
+            if del_1p19q["1p_cnvkit"] / del_1p19q["1p"][2] > 0.5 and del_1p19q["19q_cnvkit"] / del_1p19q["19q"][2] > 0.5:
+                if first_vcf:
+                    writer.write("sample\tgene(s)\tchrom\tregion\tcallers\tnormal_freq\tcopy_number")
+                    first_vcf = False
+                writer.write(f"\n{samples}\t1p19q\tNA\tNA\tcnvkit\tNA\tNA")
+            if del_1p19q["1p_gatkcnv"] / del_1p19q["1p"][2] > 0.5 and del_1p19q["19q_gatkcnv"] / del_1p19q["19q"][2] > 0.5:
+                if first_vcf:
+                    writer.write("sample\tgene(s)\tchrom\tregion\tcallers\tnormal_freq\tcopy_number")
+                    first_vcf = False
+                writer.write(f"\n{samples}\t1p19q\tNA\tNA\tgatk_cnv\tNA\tNA")
 
-    for input_vcf in input_vcfs:
-        gene_variant_dict = {}
-        log.info(f"Opening vcf file: {input_vcf}")
-        variants = VariantFile(input_vcf)
-        samples = list(variants.header.samples)
-        if len(samples) > 1:
-            raise Exeception(f"Unable to process vcf with more then one sample: {samples}")
-        else:
-            samples = samples[0]
-        log.info(f"Opening output tsv file: {output_txt}")
-        counter = 0
-        output_mode = "a"
-        if first_vcf:
-            output_mode = "w"
-        with open(output_txt, output_mode) as writer:
+        for input_vcf in input_vcfs:
+            gene_variant_dict = {}
+            log.info(f"Opening vcf file: {input_vcf}")
+            variants = VariantFile(input_vcf)
+            samples = list(variants.header.samples)
+            if len(samples) > 1:
+                raise Exeception(f"Unable to process vcf with more then one sample: {samples}")
+            else:
+                samples = samples[0]
+            counter = 0
             if first_vcf:
                 writer.write("sample\tgene(s)\tchrom\tregion\tcallers\tnormal_freq\tcopy_number")
             for variant in variants:
