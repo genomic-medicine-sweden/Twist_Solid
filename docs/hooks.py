77853c8a6c6640186da70aa5e1e7e9e941ea1ab4
Patrik Smeds <patrik.smeds@gmail.com>
2023-04-27 14:14:24 +0200

    docs: display current config and resource file as a expandable section

--- docs/hooks.py
+++ docs/hooks.py
@@ -3,4 +3,6 @@ import shutil
 def copy_changelog_and_license(*args, **kwargs):
     shutil.copy("CHANGELOG.md", "docs/changelog.md")
     shutil.copy("LICENSE.md", "docs/license.md")
+    shutil.copy("config/config.yaml", "docs/includes/config.yaml")
+    shutil.copy("config/resources.yaml", "docs/includes/resources.yaml")
     
