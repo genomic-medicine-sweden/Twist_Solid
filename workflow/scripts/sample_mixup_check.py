dc7f4b4202b2762a17cc7b15daa8033054ec64eb
jonca79 <jonas.almlof@igp.uu.se>
2024-02-01 11:05:35 +0100

    fix: bugfix and improvements

--- workflow/scripts/sample_mixup_check.py
+++ workflow/scripts/sample_mixup_check.py
@@ -3,13 +3,12 @@ import os
 
 vcf_dna_filenames = snakemake.input.id_snp_vcf_dna
 vcf_rna_filenames = snakemake.input.id_snp_vcf_rna
-report = open(snakemake.ouput.mixup_report, "w")
+report = open(snakemake.output.mixup_report, "w")
 
 vcf_dict_dna = {}
 dna_samples = {}
 vcf_dict_rna = {}
 rna_samples = {}
-
 
 def read_vcf(vcf_filename, vcf_dict, samples):
     vcf = open(vcf_filename)
@@ -24,6 +23,8 @@ def read_vcf(vcf_filename, vcf_dict, samples):
             continue
         columns = line.strip().split("\t")
         chrom = columns[0][3:]
+        if chrom == "X":
+            continue
         pos = columns[1]
         key = chrom + "_" + pos
         FORMAT = columns[8].split(":")
@@ -54,7 +55,7 @@ for rna_sample in rna_samples:
                 rna_samples[rna_sample][dna_sample] += 1
             i += 1
 
-report.write("RNA_sample\tDNA_sample\tnr_matches\t%_match\n")
+report.write("RNA_sample\tDNA_sample\tnr_matches\t%_match\tmatch\n")
 for rna_sample in rna_samples:
     best_dna_sample = ""
     best_gt_match = 0
@@ -62,4 +63,9 @@ for rna_sample in rna_samples:
         if rna_samples[rna_sample][dna_sample] > best_gt_match:
             best_dna_sample = dna_sample
             best_gt_match = rna_samples[rna_sample][dna_sample]
-    report.write(f"{rna_sample}\t{best_dna_sample}\t{best_gt_match}\t{round(best_gt_match * 100 / 44.0, 1)}%\n")
+    p_match = round(best_gt_match * 100 / 42.0, 1
+    report.write(f"{rna_sample}\t{best_dna_sample}\t{best_gt_match}\t{p_match}%")
+    if p_match > 80:
+        report.write(f"yes\n")
+    else:
+        report.write(f"no\n")
